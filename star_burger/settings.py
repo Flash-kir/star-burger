e94f655c26f6d0c95d27e749da28b539324b5793
Flash-kir <flash-kir@yandex.ru>
2024-06-20 14:46:18 +0300

    add host

--- star_burger/settings.py
+++ star_burger/settings.py
@@ -15,7 +15,7 @@ API_YANDEX_KEY = env('API_YANDEX_KEY')
 SECRET_KEY = env('SECRET_KEY')
 DEBUG = env.bool('DEBUG', False)
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['127.0.0.1', 'localhost'])
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['127.0.0.1', 'localhost', '185.221.153.31'])
 
 INSTALLED_APPS = [
     'foodcartapp.apps.FoodcartappConfig',
