b05e60feb5d1ed7b29feea06677500728cd5d9c1
Flash-kir <flash-kir@yandex.ru>
2024-08-05 11:56:48 +0300

    add domain starburger.flash-kir.ru

--- star_burger/settings.py
+++ star_burger/settings.py
@@ -15,7 +15,7 @@ API_YANDEX_KEY = env('API_YANDEX_KEY')
 SECRET_KEY = env('SECRET_KEY')
 DEBUG = env.bool('DEBUG', False)
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['127.0.0.1', 'localhost', 'kx31.deserv.net', '185.221.153.31'])
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['127.0.0.1', 'localhost', 'starburger.flash-kir.ru', '185.221.153.31'])
 
 INSTALLED_APPS = [
     'foodcartapp.apps.FoodcartappConfig',
