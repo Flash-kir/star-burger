84513ee3a6d1d4c52e540f6f7b87193eb210058d
Flash-kir <flash-kir@yandex.ru>
2024-08-05 11:13:53 +0300

    switch to PostgreSQL DB

--- star_burger/settings.py
+++ star_burger/settings.py
@@ -95,9 +95,14 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 DATABASES = {
-    'default': dj_database_url.config(
+    'sqlite3': dj_database_url.config(
         default='sqlite:////{0}'.format(os.path.join(BASE_DIR, 'db.sqlite3'))
-    )
+    ),
+    'default': dj_database_url.config(
+        default=env('DB_URL_POSTGRESQL'),
+        conn_max_age=600,
+        conn_health_checks=True,
+    ),
 }
 
 AUTH_PASSWORD_VALIDATORS = [
