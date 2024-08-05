e19a9bcef77b817b8313259e238a84dabbf83967
Flash-kir <flash-kir@yandex.ru>
2024-08-05 11:15:17 +0300

    delete sqllite settings

--- star_burger/settings.py
+++ star_burger/settings.py
@@ -95,9 +95,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 DATABASES = {
-    'sqlite3': dj_database_url.config(
-        default='sqlite:////{0}'.format(os.path.join(BASE_DIR, 'db.sqlite3'))
-    ),
     'default': dj_database_url.config(
         default=env('DB_URL_POSTGRESQL'),
         conn_max_age=600,
